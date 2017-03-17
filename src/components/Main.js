f90982566e4aae27b6a75109c28f133984e56082
yangqh <yangqh@go-goal.com>
2017-03-17 13:06:46 +0800

    yqh

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,20 +3,33 @@ require('styles/App.less');
 
 import React from 'react';
 
-let yeomanImage = require('../images/logo.svg');
+//获取图片相关数据
+let imageDatas=require('../data/imageDatas.json');
+//利用自执行函数，将图片名信息转成URL路径信息
+imageDatas=(function genImageUrl(imageDatasArr) {
+    for(let i=0,j=imageDatasArr.length;i<j;i++){
+        let singleImageData=imageDatasArr[i];
+
+        singleImageData.imageUrl=require('../images/'+singleImageData.fileName);
+        imageDatasArr[i] =singleImageData;
+    }
+    return imageDatasArr;
+})(imageDatas)
+
+
+
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> 123to get started!</div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="index">
+                <div className="notice">
+                </div>
+            </div>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
