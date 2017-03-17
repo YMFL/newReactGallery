3ce88b15c74f4c69f1663ddb3f71fc068310a0a3
yangqh <yangqh@go-goal.com>
2017-03-17 09:55:05 +0800

    yqh

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,16 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.less');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let yeomanImage = require('../images/logo.svg');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <div className="notice">Please edit <code>src/components/Main.js</code> 123to get started!</div>
       </div>
     );
   }
